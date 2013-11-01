a6ee7148701080f9d769eff07880f7dfb5155212
Adam Zaninovich <adam.zaninovich@activenetwork.com>
2013-11-01 15:07:43 -0700

    makes images work!!

--- Hamilton/Models/Event.m
+++ Hamilton/Models/Event.m
@@ -15,9 +15,15 @@
   NSMutableArray *events = [[NSMutableArray alloc] init];
   
   for (int i = 0; i < 50; i++) {
+    NSArray *images = @[@"http://i.imgur.com/ZefykCr.jpg",
+                        @"http://i.imgur.com/Ks8IFvD.jpg",
+                        @"http://i.imgur.com/C4iX3GQ.jpg",
+                        @"http://i.imgur.com/WjllUXc.jpg",
+                        @"http://i.imgur.com/dq88KFa.png"];
     NSDictionary *attributes = @{
                                  @"name" : [NSString stringWithFormat:@"Event %d", i+1],
-                                 @"startDate" : [NSDate date]
+                                 @"startDate" : [NSDate date],
+                                 @"imageURL" : [images objectAtIndex:i % 5]
                                  };
     Event *event = [[Event alloc] initWithDictionary:attributes];
     [events addObject:event];
@@ -29,6 +35,7 @@
 - (id)initWithDictionary:(NSDictionary *)attributes{
   self.name = attributes[@"name"];
   self.startDate = attributes[@"startDate"];
+  self.imageURL = attributes[@"imageURL"];
   return self;
 }
 
