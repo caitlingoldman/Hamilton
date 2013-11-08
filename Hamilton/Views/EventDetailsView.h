2feec9e082f63a0458343bf420962e4626eb154c
Adam Zaninovich <adam.zaninovich@activenetwork.com>
2013-11-08 08:24:33 -0800

    updates ed page

---
+++ Hamilton/Views/EventDetailsView.h
@@ -0,0 +1,13 @@
+//
+//  EventDetailsView.h
+//  Hamilton
+//
+//  Created by Zaninovich, Adam on 11/7/13.
+//  Copyright (c) 2013 Caitlin and Adam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventDetailsView : UIView
+
+@end
